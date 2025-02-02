128a8c2dfb99dc2f3ecba060697124a472a26664
Felix Idalia <flxidalia@gmail.com>
2025-02-02 13:32:16 +0300

    Modified: Added error message if users not found

--- src/crud/readAll.js
+++ src/crud/readAll.js
@@ -3,8 +3,11 @@ const User = require("../model/Users");
 const readAll = async (req, res) => {
    try {
         const users = await User.find();
-        if (!users) return res.status(404).json({ message: "No records found" });
-        return res.status(200).json(users);
+
+        if (!users) {
+          return res.status(404).json({ message: "No records found" });
+        }
+        return res.status(200).json({ message: "success", users });
 
    } catch (error) {
         return res.status(500).json({ error: error.message });
