7de5e6ab74c6dd5e9f16929c7955e90fec761a32
Felix Idalia <flxidalia@gmail.com>
2025-02-02 13:35:54 +0300

    Modified: Added error message if updatedUser not found

--- src/crud/updatePut.js
+++ src/crud/updatePut.js
@@ -3,7 +3,11 @@ const User = require("../model/Users");
 const updatePut = async (req, res) => {
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).json(updatedUser);
+
+        if (!updatedUser) {
+            res.status(404).json({ error: "User not found "});
+        }
+        res.status(200).json({ message: "User record updated successfully", updatedUser });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
