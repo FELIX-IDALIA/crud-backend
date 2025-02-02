0b2e7976dfaec7f6e263e66342122914748c2e89
Felix Idalia <flxidalia@gmail.com>
2025-02-02 13:33:28 +0300

    Modified: Added error message if user not found

--- src/crud/readOne.js
+++ src/crud/readOne.js
@@ -8,7 +8,7 @@ const readOne = async (req, res) => {
             return res.status(404).json({ error: "User not found" });
         }
 
-        return res.status(200).json(user);
+        return res.status(200).json({ message: "success", user });
 
     } catch (error) {
         return res.status(500).json({ error: error.message });
