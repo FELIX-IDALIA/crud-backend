afdc12c5240576eb6803f9a7515f29397f4111e3
Felix Idalia <flxidalia@gmail.com>
2025-02-03 16:12:01 +0300

    Modified: Added more fields and custom message to UserSchema

--- src/model/Users.js
+++ src/model/Users.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    name: {type: String, required: [true, "name is required"]}, // custom error message
+    username: {type: String, required: [true, "username is required"]}, // custom error message
+    email: {type: String, required: [true, "email is required"]},   // custom error message
     age: {type: Number, required: true},
+    gender: String,
     country: String,
     city: String
+    
 
-}, { timestamps: true });
+}, { timestamps: true }
+);
+
+// create a compound index for all unique fields
+UserSchema.index({ username: 1}, { unique: true });
+UserSchema.index({ email: 1}, { unique: true });
 
 const User = mongoose.model("Users", UserSchema);
 
