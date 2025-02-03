7dda240fc81cae1cf2c909d15d7fcc13b2a8d587
Felix Idalia <flxidalia@gmail.com>
2025-02-03 18:23:21 +0300

    Modified: Added route

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,6 +1,6 @@
 const create = require("../crud/create");
 const readOne = require("../crud/readOne");
-const readAll = require("../crud/readAll");
+const {readAll, readUsersAboveTen} = require("../crud/readAll");
 const updatePatch = require("../crud/updatePatch");
 const updatePut = require("../crud/updatePut");
 const deleteOne = require("../crud/deleteOne");
@@ -11,6 +11,7 @@ const router = express.Router();
 router.post("/create-user", create);
 router.get("/read-one/:id", readOne);
 router.get("/read-all", readAll);
+router.get("/read-users-above", readUsersAboveTen);
 router.patch("/update-by-patch/:id", updatePatch);
 router.put("/update-by-put/:id", updatePut);
 router.delete("/delete-one/:id", deleteOne);
