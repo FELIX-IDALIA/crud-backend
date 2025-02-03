b14594f467584faea9a2d1eee56f88ba472111a4
Felix Idalia <flxidalia@gmail.com>
2025-02-03 16:10:40 +0300

    Modified: included error handling if required field is missing

--- src/crud/create.js
+++ src/crud/create.js
@@ -10,9 +10,21 @@ const create = async (req, res) => {
         return res.status(201).json({ message: "User created successfully!"});
 
     } catch (error) {
-        console.error("Error creating user:", error);
-        return res.status(500).json({ message: "Failed to create user!"});
-    }
+        if (error.name === "ValidationError") {
+            // Extract validation error message
+            const field = Object.keys(error.errors)[0]; // Get the field name (e.g., "username")
+            const message = error.errors[field].message; // Get the custom message
+            return res.status(400).json({ "Validation error": message});
+        } 
+
+        if (error.code === 11000) { // MongoDB duplicate key error
+            const field = Object.keys(error.keyPattern)[0]; // Get the field causing the error
+            return res.status(400).json({ error: `${field} already exists` });
+        }
+        
+        return res.status(500).json({ "Unknown error": error});
+    
+ }
 };
 
 module.exports = create;
