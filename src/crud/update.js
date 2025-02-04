681054bee0d8121d9482f69919679403f0bfcf34
Felix Idalia <flxidalia@gmail.com>
2025-02-04 16:39:55 +0300

    Modified: Added a function to check if username or email exists

--- src/crud/update.js
+++ src/crud/update.js
@@ -1,7 +1,36 @@
 const User = require("../model/Users");
+
+// Check if username or email already exists in another user's document
+const checkUniqueFields = async (req, id) => {
+    try {
+        const { username, email } = req.body;
+
+        const existingUser = await User.findOne({
+            $or: [{ username}, { email }],
+            _id: { $ne: id } // Exclude current user
+        });
+
+        if (existingUser) {
+            return { error: "Username or Email already exists. Please choose a diffferent one"};
+        }
+
+        return null; // No duplicate found
+
+    } catch (error) {
+        return { error: error.message }; // Return error as an object instead of sending response directly
+    }
+};
+
 // Finds user by id and update (PUT)
 const updatePut = async (req, res) => {
     try {
+        // Call checkUniqueFields and wait for result
+        const uniqueCheckResult = await checkUniqueFields(req, req.params.id);
+
+        if (uniqueCheckResult) {
+            return res.status(400).json(uniqueCheckResult);
+        }
+
         const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
         if (!updatedUser) {
@@ -17,6 +46,8 @@ const updatePut = async (req, res) => {
 // Find user by id and update (PATCH)
 const updatePatch = async (req, res) => {
     try {
+        checkUniqueFields();
+
         const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
         if (!updatedUser) {
