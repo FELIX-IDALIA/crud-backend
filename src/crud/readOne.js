dc843818f60b76571678a7742527e0ae27c72165
Felix Idalia <flxidalia@gmail.com>
2025-02-04 12:07:33 +0300

    Modified: Added a function, finds user by email

--- src/crud/readOne.js
+++ src/crud/readOne.js
@@ -17,4 +17,23 @@ const readOne = async (req, res) => {
 
 };
 
-module.exports = readOne;
+// Read by email
+const readByEmail = async (req, res) => {
+    try {
+        // Extract email from query parameters
+        const { email } = req.query;
+        // Find user by email and return only name and email
+        const user = await User.findOne({ email }).select("name email");
+
+        if (!user) {
+            res.status(404).json({ error: "User with this email does not exist" });
+        }
+
+            res.status(200).json({ message: "Success", user });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { readByEmail, readOne };
